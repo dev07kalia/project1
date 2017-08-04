3c77847e7af658598823dbe24aaf1647d4ad530a
Dev Vrat Kalia <you@example.com>
2017-08-04 12:38:55 +0530

    concatination concept completed..

---
+++ spychat/main.py
@@ -0,0 +1,4 @@
+print "Let's get started"
+spy_name = raw_input("whats your name ?")
+spy_salutation = raw_input("what should i call you Mr. or Miss?")
+print "welcome " + spy_salutation + spy_name + " glad to have you on board "
